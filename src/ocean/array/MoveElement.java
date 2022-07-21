4fd4532634bd9c4dcb146c0111e622ba74845714
Ocean <2080917477@qq.com>
2022-07-21 12:48:06 +0800

    快慢指针

--- src/ocean/array/MoveElement.java
+++ src/ocean/array/MoveElement.java
@@ -1,5 +1,18 @@
 package ocean.array;
+// https://leetcode.cn/problems/remove-element/
 
 public class MoveElement {
+    public int removeElement(int[] nums, int val) {
+        // 快慢指针，相对运动
+        int fastIndex = 0;
+        int slowIndex;
+        for (slowIndex = 0; fastIndex < nums.length; fastIndex++) {
+            if (nums[fastIndex] != val) {
+                nums[slowIndex] = nums[fastIndex];
+                slowIndex++;
+            }
+        }
+        return slowIndex;
 
+    }
 }
