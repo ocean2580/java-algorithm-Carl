b24ea404183bf5fa289810fa2ee31d952a2519a6
Ocean <2080917477@qq.com>
2022-07-22 14:21:56 +0800

    1.有序数组的平方和序 2.添加测试类

---
+++ src/ocean/array/SquareSorted.java
@@ -0,0 +1,34 @@
+package ocean.array;
+
+import java.util.Arrays;
+
+public class SquareSorted {
+    public int[] doublePointer(int[] nums) {
+        int right = nums.length - 1;
+        int left = 0;
+        int[] result = new int[nums.length];
+        int index = result.length - 1;
+
+        while (left <= right) { // 两头趋中
+            if (nums[left] * nums[left] > nums[right] * nums[right]) {
+                // 正数的相对位置是不变的， 需要调整的是负数平方后的相对位置
+                result[index--] = nums[left] * nums[left];
+                ++left;
+            } else {
+                result[index--] = nums[right] * nums[right];
+                --right;
+            }
+        }
+        return result;
+    }
+
+    public int[] easy(int[] nums) { // 暴力
+        int[] result = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            result[i] = nums[i]*nums[i];
+        }
+        Arrays.sort(result);
+        return result;
+    }
+
+}
---
+++ src/ocean/array/Test.java
@@ -0,0 +1,9 @@
+package ocean.array;
+
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
