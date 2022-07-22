aa7145d39fe8f282561f78315c10889f456673ad
Ocean <2080917477@qq.com>
2022-07-22 14:58:13 +0800

    最小子序列长度（滑窗法）

---
+++ src/ocean/array/MinSubArrLen.java
@@ -0,0 +1,34 @@
+package ocean.array;
+
+public class MinSubArrLen {
+    public int slideWindow(int s, int[] nums) {
+        int left = 0;
+        int sum = 0;
+        int result = Integer.MAX_VALUE;
+
+        for (int right = 0; right < nums.length; right++) {
+            sum += nums[right];
+            while (sum >= s) {
+                result = Math.min(result, right - left + 1);
+                sum -= nums[left++];    // 移动
+            }
+        }
+
+        return result == Integer.MAX_VALUE ? 0 : result;
+    }
+
+    public int easy(int s, int[] nums) {
+        int result = Integer.MAX_VALUE;
+        int temp;
+
+        for (int i = 0; i < nums.length; i++) {
+            temp = nums[i];
+            for (int j = i+1; j<nums.length; j++) {
+                if (temp < s) temp += nums[j];
+                else result = Math.min(result, j - i);  // 比较
+            }
+        }
+
+        return result == Integer.MAX_VALUE ? 0 : result;
+    }
+}
